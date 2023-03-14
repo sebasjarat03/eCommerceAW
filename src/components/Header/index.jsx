fa10e63afa9e40fb987f149a87045f8bf429c835
Sebastian Jaramillo <sebastian.jaramillo2@u.icesi.edu.co>
2023-03-14 18:56:34 -0500

    header component created

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <h1>eCommerce</h1>
+            <button>Log In</button>
+        </header>
+    )
+}
+
+export default Header
