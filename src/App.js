2c7b369c2c1b2e6e60b8a28f6377afbed357ba87
Sebastian Jaramillo <sebastian.jaramillo2@correo.icesi.edu.co>
2023-03-25 21:30:29 -0500

    Login button now working, including logout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Card from './components/Card';
 
 function App() {
   return (
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Header() {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleLogIn = () => {
+        setIsLoggedIn(true);
+    }
+
+    const handleLogOut = () => {
+        setIsLoggedIn(false);
+    }
+
     return (
         <header>
-            <h1>eCommerce</h1>
-            <button>Log In</button>
+            <nav class="navbar bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand">eCommerceAW</a>
+                    {isLoggedIn ? (
+
+                        <div class="card">
+                            <div>
+                                <span style={{ marginRight: '10px', marginLeft: '10px' }}>John Doe</span>
+                                <button onClick={() => handleLogOut()} type="button" class="btn btn-outline-secondary" >Log Out</button>
+                            </div>
+                        </div>
+
+                    ) : (
+                        <button onClick={() => handleLogIn()} type="button" class="btn btn-outline-secondary">Log In</button>
+                    )}
+                </div>
+            </nav>
+
+
         </header>
     )
 }
